b5b1cf30b0281dc4c9e9f2e5d88185e8112a9f10
greg <grzegorz.kozub@gmail.com>
2024-08-17 11:49:49 +0200

    wip

--- update.zsh
+++ update.zsh
@@ -2,46 +2,29 @@
 
 [[ $(pacman -Qs kepubify-bin) ]] || paru -S --aur --noconfirm kepubify-bin
 
-for BOOK in \
-  'Asynchronous Programming in Rust' \
-  'Async programming in Rust with async-std' \
-  'Command Line Applications in Rust' \
-  'Comprehensive Rust 🦀' \
-  'CXX-Qt Documentation' \
-  'CXX Documentation' \
-  'FLTK-rs Book.epub' \
-  'Learning Rust With Entirely Too Many Linked Lists' \
-  'mdBook Documentation' \
-  'Rust and WebAssembly' \
-  'Rust API Guidelines' \
-  'Rust By Example' \
-  'Rust Compiler Development Guide' \
-  'Rust Cookbook' \
-  'Rust Design Patterns' \
-  'The Cargo Book' \
-  'The Embedded Rust Book' \
-  'The Little Book of Rust Macros' \
-  'The Rustdoc Book' \
-  'The Rust Edition Guide' \
-  'The Rust on ESP Book' \
-  'The Rustonomicon' \
-  'The Rust Performance Book' \
-  'The Rust Programming Language' \
-  'The Rust Reference'
+for BOOK in $(
+  curl --silent https://dieterplex.github.io/rust-ebookshelf/ |
+    grep -e '[^"]*.epub' --only-matching
+  )
 do
 
-  URI=$(echo $BOOK | jq --raw-input --raw-output '@uri')
+  # URI=$(echo $BOOK | jq --raw-input --raw-output '@uri')
   curl --silent \
-    "https://dieterplex.github.io/rust-ebookshelf/$URI.epub" \
-    > "$BOOK.epub"
+    "https://dieterplex.github.io/rust-ebookshelf/$BOOK" \
+    > "$BOOK"
 
-  kepubify \
-    --inplace "$BOOK.epub" \
-    --css 'code { font-family: Cascadia Code; }' \
-    > /dev/null
-
-  cp "$BOOK.kepub.epub" /run/media/$USER/KOBOeReader
-  rm "$BOOK.epub"
+  # kepubify \
+  #   --inplace "$BOOK" \
+  #   --css 'code { font-family: Cascadia Code; }' \
+  #   > /dev/null
+  #
+  # cp "$BOOK.kepub.epub" /run/media/$USER/KOBOeReader
+  # rm "$BOOK"
 
 done
+
+
+# curl --silent https://dieterplex.github.io/rust-ebookshelf/ | grep -e '[^<td>][^<]*<a'
+#curl --silent https://dieterplex.github.io/rust-ebookshelf/ | grep -e 'epub' | sed 's/<tr><td>[0-9]*<\/td><td>//'
+#curl --silent https://dieterplex.github.io/rust-ebookshelf/ | hxnormalize -x | hxselect -c 'tbody tr td:nth-child(2):nth-child(2)' -s '\n'
 
