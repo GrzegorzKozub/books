56e619e00f0590585d09ced78847acb3cb8c2df7
greg <grzegorz.kozub@gmail.com>
2024-09-17 08:31:52 +0200

    more better

--- update.zsh
+++ update.zsh
@@ -24,9 +24,11 @@ echo $BOOKS | while read -r BOOK; do
   kepubify \
     --inplace "$BOOK.epub" \
     --css 'html { font-family: Georgia !important; font-size: 16px; }' \
-    --css 'code, p code, pre code, ul li code { color: #111 !important; font-family: Cascadia Code !important; font-size: 15px; }' \
+    --css 'code, p code, pre code, ul li code, span.caption code, a code span { color: #202020 !important; font-family: Cascadia Code; font-size: 15px; }' \
     --css 'h1 code, h2 code, h3 code, h4 code, h5 code, h6 code { font-size: 1em; }' \
-    --css 'span.caption, span.caption code { color: #444 !important; font-weight: normal; }' \
+    --css 'h1 code span, h2 code span, h3 code span, h4 code span, h5 code span, h6 code span { font-family: Georgia; }' \
+    --css 'span.caption { color: #404040; font-weight: normal; }' \
+    --css 'a span { color: #404040; }' \
     > /dev/null
   rm "$BOOK.epub"
 
