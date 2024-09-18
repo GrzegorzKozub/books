3c7980b01e9f745fa38b3c4b23c6db7cf0207f4d
greg <grzegorz.kozub@gmail.com>
2024-09-18 07:55:21 +0200

    are we done yet

--- update.zsh
+++ update.zsh
@@ -24,7 +24,8 @@ echo $BOOKS | while read -r BOOK; do
   kepubify \
     --inplace "$BOOK.epub" \
     --css 'html { font-family: Georgia !important; font-size: 16px; }' \
-    --css 'code, p code, pre code, ul li code, span.caption code, a code span { color: #202020 !important; font-family: Cascadia Code; font-size: 15px; }' \
+    --css 'code, p code, pre code, ul li code { color: #000 !important; font-family: Cascadia Code; font-size: 15px; }' \
+    --css 'span.caption code, a code span { color: #404040 !important; font-family: Cascadia Code; font-size: 15px; }' \
     --css 'h1 code, h2 code, h3 code, h4 code, h5 code, h6 code { font-size: 1em; }' \
     --css 'h1 code span, h2 code span, h3 code span, h4 code span, h5 code span, h6 code span { font-family: Georgia; }' \
     --css 'span.caption { color: #404040; font-weight: normal; }' \
